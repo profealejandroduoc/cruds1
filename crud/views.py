2f2289abe7c7ec3e9b1fc19e22bdd68a9cbd71f2
El profe <al.sepulvedam@profesor.duoc.cl>
2024-06-05 15:22:44 -0400

    index con datos

--- crud/views.py
+++ crud/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from datetime import date
 
 # Create your views here.
 def index(request):
-    return render(request,'crud/index.html')
+    fecha=date.isoformat(date.today())
+    texto="Para traer los datos desde la vista se debe enviar a través del contexto de datos"
+    lista=['Alfajor', 'Poleron', 'Paraguas','Gorro','Cruz con Micrófono']
+    elementos=len(lista)
+
+    datos={
+        "fecha":fecha,
+        "msg":texto,
+        "lista":lista,
+        "items":elementos
+    }
+
+
+
+    return render(request,'crud/index.html', datos)
